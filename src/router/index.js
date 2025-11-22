3c45ab012973dc353bc045293f5100e3cba34a58
FessorFace <amanda.h.hatton@gmail.com>
2025-11-21 19:38:19 -0900

    All files for adding routing to quiz start pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 //define routing rules//
 import { createRouter, createWebHistory } from "vue-router"
-import HomeView from "../views/HomeView.vue"
+import HomeView from "../views/HomeView.vue" //tutorial calls this QuizesView
 import QuizView from "../views/QuizView.vue"
 
 
